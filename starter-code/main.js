1c2d0d5ed635943c096af73e465baaa0b4daa167
Cara Chew <Cara Chew>
2017-03-08 11:05:33 +0800

    Created Game Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,9 @@
 console.log("JS file is connected to HTML! Woo!")
+
+//Defining card variables to Smiles Memory Game//
+
+var cardOne="queen";
+var cardTwo="queen";
+var cardThree="king";
+var cardFour="king";
+
